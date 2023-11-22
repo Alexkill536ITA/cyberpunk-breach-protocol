2dc7f74b6a68539ec30654fb5c8ff0585b7ebed1
Alexkill536ITA <manzo.alizzi@libero.it>
2023-11-22 21:22:17 +0100

    remove not used import

--- src/app/layout/game/game.component.ts
+++ src/app/layout/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Challenge, Level, Matrix } from 'src/app/models/comon.model';
 import { LevelGeneratorService } from 'src/app/service/level-generator.service';
--- src/app/modules/maker-level/maker-level.component.ts
+++ src/app/modules/maker-level/maker-level.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, UntypedFormArray, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subject } from 'rxjs';
 import { Level } from 'src/app/models/comon.model';
